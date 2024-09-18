6d33c994051abf2b9dc7de6342701a955599cdf7
Anatoly Chizhikov <78175165+Tolinme@users.noreply.github.com>
2024-09-18 13:18:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+let targetUrl = 'https://www.vgtk.by/schedule/lessons/day-today.php'
+const mainElement = document.querySelector('.main')
+const date = document.querySelector(".date")
+const selectElement = document.querySelector("select[name='lessons']")
+const userSelectedGropus = []
+const SСHEDULE = []
+
+const allGroups = [
+  "А-11","А-21","А-31","А-41",
+  "Д-11","Д-31","ЖБИ-11","ЖБИ-21",
+  "С-11","С-21","С-31","С-41",
+  "С-42","ТС-11",
+
+  "Б-31","ВР-11","ВР-21","ВС-21",
+  "ВС-31","ВС-41","ИТ-11","ИТ-21",
+  "М-11","М-21","Э-11","Э-21",
+  "Э-32","Э-42","ТЭ-11",
+
+  "АС-22","АС-32","АС-42","ОС-11",
+  "ОС-21","ОС-32","ОС-42","ЭМ-11",
+  "ЭМ-21","ЭМ-32","ЭМ-42",
+
+  "ПКО-11","ПКО-21","ПКО-31","ПКЭ-39",
+  "ПСМ-13","ПСМ-23","ПСМ-33","ПСМ-33",
+  "ПФЭ-16","ПФЭ-26","ПФЭ-36",
+
+  "ПОО-14","ПОО-24","ПОО-34","ПТЭ-111",
+  "ПМЭ-18","ПМЭ-28","ПМЭ-38",
+
+
+  "ПТС-17","ПТС-27","ПТС-217","ПТС-37",
+  "ПТС-317","ПФС-32","ПСЭ-11",
+
+  "ПЭС-15","ПЭС-115","ПЭС-25","ПЭС-35",
+  "ПЭС-315","ПКМ-12","ПКМ-22","ПМР-19",
+  "ПМР-119","ПМР-29",
+  ]
+
+allGroups.forEach(group => {selectElement.appendChild(new Option(group, group))})
+
+function removeStyleAttribute(node) {
+  if (node.nodeType === 1) {node.removeAttribute("style")}
+  for (const child of node.childNodes){removeStyleAttribute(child)}
+}
+
+// Функция для разделения td с атрибутом rowspan равным 2
+function splitRowspan2TD(tableElement) {
+    for (let i = 0; i < tableElement.rows.length; i++) {
+        let row = tableElement.rows[i];
+        for (let j = 0; j < row.cells.length; j++) {
+            let cell = row.cells[j];
+            if (cell.hasAttribute("rowspan") && parseInt(cell.getAttribute("rowspan")) === 2) {
+                let newCell = cell.cloneNode(true);
+                cell.removeAttribute("rowspan");
+                newCell.removeAttribute("rowspan");
+                if (row.nextElementSibling) {
+                    let nextRow = row.nextElementSibling;
+                    let nextCell = nextRow.insertCell(j);
+                    nextCell.innerHTML = newCell.innerHTML;
+                }
+            }
+        }
+    }
+}
+
+fetch(proxyUrl + targetUrl)
+  .then(response => response.text())
+  .then(data => {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = data;
+    const tableElement = tempElement.querySelector('table');
+    date.innerText = tableElement.rows[0].innerText.trim()
+    
+    mainElement.append(tableElement)
+    splitRowspan2TD(tableElement)
+    removeStyleAttribute(mainElement);
+    
+    for (let i = 0; i < tableElement.rows.length - 11; i++) {
+        const row = tableElement.rows[i];
+        if (row.cells.length > 1) {
+            Array.from(row.cells).forEach((cell, j) => {
+                let cellValue = cell.innerText.trim();
+                if (allGroups.includes(cellValue)) {
+                    const groupSchedule = {
+                        groupName: cellValue,
+                        ...Array.from({ length: 11 }, (_, index) => ({
+                            [index + 1]: {
+                                lessonName: tableElement.rows[i + index + 1].cells[j]?.innerText.trim(),
+                                cabinet: tableElement.rows[i + index + 1].cells[j + 1]?.innerText.trim()
+                            }
+                        }))
+                    }
+                    SСHEDULE.push(groupSchedule);
+                }
+            })
+        }
+    }
+
+    console.log(SСHEDULE)
+  })
+  .catch(error => console.error('Ошибка:', error))
+
+const changeDay = () => {
+    targetUrl = "https://www.vgtk.by/schedule/lessons/day-tomorrow.php";
+};
+
+
+
+
+
+
